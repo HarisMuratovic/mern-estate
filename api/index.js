0e4b5738c1e7c155ffe83678016a7c1124837858
Haris Muratović <haris.muratovic@student.uninp.edu.rs>
2024-01-26 01:46:12 +0100

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
 dotenv.config();
 
 mongoose
@@ -12,10 +13,11 @@ mongoose
       console.log(err);
     });
 
-
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is runing on port 3000!');
-    }
-);
+    });
+
+
+    app.use("/api/user", userRouter)
