407bb4e65e77f284419edee17112bba964b84889
Haris Muratović <haris.muratovic@student.uninp.edu.rs>
2024-02-02 04:02:14 +0100

    complete profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
-import SignUp from './pages/SignUP';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -9,15 +9,15 @@ import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Sign-in" element={<SignIn />} />
-        <Route path="/Sign-up" element={<SignUp />} />
-        <Route path="/about" element={<About />} />
-        <Route element={<PrivateRoute />} >
-        <Route path="/profile" element={<Profile />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
         </Route>
       </Routes>
     </BrowserRouter>
