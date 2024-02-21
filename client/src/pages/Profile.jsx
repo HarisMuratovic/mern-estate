5a260c7d29e292e33806e24f5c56006e17110901
Haris Muratović <haris.muratovic@student.uninp.edu.rs>
2024-02-21 03:24:50 +0100

    complete update listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,7 +17,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser, loading, error } = useSelector((state) => state.user);
@@ -113,7 +113,7 @@ export default function Profile() {
 
   const handleSignOut = async () => {
     try {
-      dispatch(signOutUserStart())
+      dispatch(signOutUserStart());
       const res = await fetch('/api/auth/signout');
       const data = await res.json();
       if (data.success === false) {
@@ -130,7 +130,7 @@ export default function Profile() {
     try {
       setShowListingsError(false);
       const res = await fetch(`/api/user/listings/${currentUser._id}`);
-      const data = await res.json();  
+      const data = await res.json();
       if (data.success === false) {
         setShowListingsError(true);
         return;
@@ -142,7 +142,7 @@ export default function Profile() {
     }
   };
 
-  const handListingDelete = async (listingId) => {
+  const handleListingDelete = async (listingId) => {
     try {
       const res = await fetch(`/api/listing/delete/${listingId}`, {
         method: 'DELETE',
@@ -154,11 +154,12 @@ export default function Profile() {
       }
 
       setUserListings((prev) =>
-        prev.filter((listing) => listing._id !== listingId));
+        prev.filter((listing) => listing._id !== listingId)
+      );
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -218,9 +219,9 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
-        <Link 
-          className='bg-green-700 text-white p-3 rounded-lg upercase text-center hover:opacity-95'
-          to={"/create-listing"}
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
         >
           Create Listing
         </Link>
@@ -248,11 +249,12 @@ export default function Profile() {
         {showListingsError ? 'Error showing listings' : ''}
       </p>
 
-      {userListings &&
-        userListings.length > 0 &&
-        <div className="flex flex-col gap-4">
-          <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
-          {userListings.map((listing) => ( 
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
             <div
               key={listing._id}
               className='border rounded-lg p-3 flex justify-between items-center gap-4'
@@ -265,19 +267,27 @@ export default function Profile() {
                 />
               </Link>
               <Link
-                className='text-slate-700 font-semibold hover:underline truncate flex-1'
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
                 to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
-  
-              <div className='flex flex-col ite,'>
-                <button onClick={()=>handListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
-                <button className='text-green-700 uppercase'>Edit</button>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
               </div>
             </div>
           ))}
-        </div>}
+        </div>
+      )}
     </div>
   );
 }
